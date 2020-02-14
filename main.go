7e6cddf4b9750c2bc7d0282292da6567a560c7e9
INeverSleeeep <362761534@qq.com>
2020-02-15 00:10:06 +0800

    测试通过了代理

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ import (
 
 // Proxy is url port pair
 type Proxy struct {
-	URL  string
-	Port int
+	RawURL   string
+	ProxyURL string
 }
 
 // SSL used for https
@@ -28,9 +28,9 @@ type SSL struct {
 
 // ProxyConfig Config
 type ProxyConfig struct {
-	ProxyList   []Proxy
-	SSL         *SSL
-	DefaultPort int
+	ProxyList []Proxy
+	SSL       *SSL
+	Default   string
 }
 
 var path = flag.String("config", "", "reverse proxy config file path with yaml format")
@@ -54,7 +54,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	if config.DefaultPort == 0 {
+	if len(config.Default) == 0 {
 		panic(errors.New("config file must set defaulport"))
 	}
 	http.HandleFunc("/", ServeHTTP)
@@ -67,18 +67,18 @@ func main() {
 // ServeHTTP handle http/https request
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer CatchPanic()
-	port := config.DefaultPort
+	proxyurl := config.Default
 	for _, proxy := range config.ProxyList {
-		if strings.HasPrefix(r.URL.Hostname(), proxy.URL) {
-			port = proxy.Port
+		if strings.HasPrefix(r.URL.Hostname(), proxy.RawURL) {
+			proxyurl = proxy.ProxyURL
 			break
 		}
 	}
-	remote, err := url.Parse(fmt.Sprintf("http://127.0.0.1:%d", port))
+	remote, err := url.Parse(fmt.Sprintf("http://%s/", proxyurl))
 	if err != nil {
 		panic(err)
 	}
-	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy := NewSingleHostReverseProxy(remote, proxyurl)
 	proxy.ServeHTTP(w, r)
 }
 
@@ -87,4 +87,37 @@ func CatchPanic() {
 	if err := recover(); err != nil {
 		println(err)
 	}
+}
+
+// NewSingleHostReverseProxy Copy From httputil.NewSingleHostReverseProxy And Pass host
+func NewSingleHostReverseProxy(target *url.URL, host string) *httputil.ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.Host = host
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
 }
