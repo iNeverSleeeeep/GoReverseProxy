688cbb44317b50c9b99829c652a265b4eab95908
INeverSleeeep <362761534@qq.com>
2020-02-15 10:43:23 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,5 @@ rm -f /etc/init.d/gorproxy
 rm -f /etc/gorproxy.yaml
 go build -o gorproxy main.go
 ln gorproxy /usr/bin/gorproxy
-touch /etc/init.d/gorproxy
-echo "gorproxy &" >> /etc/init.d/gorproxy
-chmod 777 /etc/init.d/gorproxy
+cp -f gorproxy /etc/init.d/gorproxy
 cp -f ./gorproxy.yaml /etc/gorproxy.yaml
